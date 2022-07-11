aa986c9b39697f95f1e0159e9ccbbf67867177cb
Rishikesh-278 <rishikeshbidkar04@gmail.com>
2022-07-12 00:35:03 +0530

    Added "as" to line 5. To avoid confusion!

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -2,7 +2,7 @@ import 'package:climate/services/location.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:climate/services/location.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 
 class LoadingScreen extends StatefulWidget {
@@ -11,10 +11,13 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
   @override
   void initState() {
     super.initState();
     getLocation();
+    print('This line of code is triggered');
+
   }
 
   Future<void> getLocation() async {
@@ -29,15 +32,28 @@ class _LoadingScreenState extends State<LoadingScreen> {
   
   Future<void> getData() async
   {
-    Response response =
+    final Uri uri = Uri.parse('https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=eaad9201c4ac58dcfd09e65779215274');
+    http.Response response = await http.get(uri);
+    print(response.body);
+    print(response.statusCode);
+
+    if(response.statusCode == 200)
+      {
+        String data = response.body;
+        print(data);
+      }
+    else
+      {
+        print(response.statusCode);
+      }
   }
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return const Scaffold();
   }
 }
 
 
 
-https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=eaad9201c4ac58dcfd09e65779215274
